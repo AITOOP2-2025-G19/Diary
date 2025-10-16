bd6439315f255b52ec341379983ecfdc2c860366
T-K-T-K-T <k24128kk@aitech.ac.jp>
2025-10-16 15:35:37 +0900

    日記の修正

--- diaries/TKTKTDiary.py
+++ diaries/TKTKTDiary.py
@@ -3,6 +3,6 @@ class TKTKTDiary(AbstractDiary):
     def get_date(self):
         return "2025-10-16"
     def get_summary(self):
-        return "説明早いよ"
+        return "難しいよーん"
     def get_author(self):
         return "TKTKT"
