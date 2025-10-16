59d8574791a0b3d74fb56324d68a69b577dd3043
T-K-T-K-T <k24128kk@aitech.ac.jp>
2025-10-16 15:14:47 +0900

    TKTKT日記追加

---
+++ diaries/TKTKTDiary.py
@@ -0,0 +1,8 @@
+from diaries.AbstractDiary import AbstractDiary
+class DiarySample(AbstractDiary):
+    def get_date(self):
+        return "2025-10-16"
+    def get_summary(self):
+        return "説明早いよ"
+    def get_author(self):
+        return "TKTKT"
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from diaries.DiarySample import DiarySample
+from diaries.TKTKTSample import DiarySample
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(), ]
